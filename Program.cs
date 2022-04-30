c11030c2c5a0fdf75c6962ccd1e72e726b55f70b
Cleydson Fernando <cleydsonfernando@gmail.com>
2022-04-30 17:35:46 -0300

    Verificar_Numero_Ipar_Par

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Verificardor_Nun_Ímpar_Par
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resp = "s";
+            while (resp == "s")
+            {
+
+                Console.WriteLine("Digite um número para verificarmos se é Ímpar ou Par: ");
+                double n1 = double.Parse(Console.ReadLine());
+                double n2 = 2;
+
+                double resultado = n1 % n2;
+                if (resultado == 0)
+                {
+                    Console.WriteLine("Número Par");
+                    Console.WriteLine("Deseja verificar outro número? (s/n)");
+                    resp = Console.ReadLine();
+                    Console.Clear();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Número Ímpar");
+                    Console.WriteLine("Deseja Deseja verificar outro número?? (s/n)");
+                    resp = Console.ReadLine();
+                    Console.Clear();
+                }
+                
+                Console.WriteLine("Fim do programa");
+            }
+
+        }
+    }
+}
